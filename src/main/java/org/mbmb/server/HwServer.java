27a7c88b13e743076c07c10e6004bb2f3a53c385
Marcus Borch <mborch@idtus.com>
2018-05-07 07:40:53 -0400

    hello world from the website

---
+++ src/main/java/org/mbmb/server/HwServer.java
@@ -0,0 +1,28 @@
+package org.mbmb.server;
+
+import org.zeromq.ZMQ;
+
+public class HwServer {
+
+	public static void main(String[] args) throws Exception {
+		try (ZMQ.Context context = ZMQ.context(1)) {
+			//  Socket to talk to clients
+			try (ZMQ.Socket responder = context.socket(ZMQ.REP)) {
+				responder.bind("tcp://*:5555");
+
+				while (!Thread.currentThread().isInterrupted()) {
+					// Wait for next request from the client
+					byte[] request = responder.recv(0);
+					System.out.println("Received " + new String(request));
+
+					// Do some 'work'
+					Thread.sleep(500);
+
+					// Send reply back to client
+					String reply = "World";
+					responder.send(reply.getBytes(), 0);
+				}
+			}
+		}
+	}
+}
